78e96ab0fbaf65c4b7740f65c788bc0be1300cd6
Jsmith2021 <league@WTS-iMac-11.attlocal.net>
2019-04-16 14:24:38 -0700

    Day 2
    :smile:

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,16 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot thanos=new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+thanos.setSpeed(99990);
 		// 5. Set the pen width to 5
-
+thanos.penDown();
+thanos.setPenWidth(5);		
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+for(int i=0;i<4;i++) {	
 
+	thanos.setRandomPenColor();
+	drawSquare();
+	thanos.turn(90);
+
+
+
+
+}
 			// 7. Set the pen color to random
-	
+
 			// 1. Call the drawSquare() method
 	
 			// 8. Turn the robot 90 degrees to the right
@@ -23,18 +33,28 @@ public class FourSquare {
 	}
 
 	
-	void drawSquare() {
+	void drawSquare() { 
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
+		for(int i=0;i<4;i++) {
+			thanos.move(200);
+			thanos.turn(90);	
+		}
 		
 	}
 
 	public static void main(String[] args) {
 		new FourSquare().go();
-	}
+
+		
 
 }
+	
+}	
+	
+
+		
+
 
 
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot thanos=new Robot();
+	thanos.penDown();
+	thanos.setSpeed(10000);
+	for(int i=0;i<4;i++) {
+	thanos.move(200);
+	thanos.turn(90);
+
 	}
-}
+	
+	}
+	}
+
