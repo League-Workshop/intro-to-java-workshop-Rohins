891b3482973745081a75d86f9215a5c678115e8c
Rohins <league@WTS-iMac-11.attlocal.net>
2019-04-17 14:23:19 -0700

    Day 3
    :smile:

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,23 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=0;
 		// 2.  Ask the user a question 
-		
+		String anwser1=JOptionPane.showInputDialog("Does curry taste good?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(anwser1.equalsIgnoreCase("no")){
+			JOptionPane.showMessageDialog(null, "Correct!");
+			JOptionPane.showMessageDialog(null, "score=1");}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+			JOptionPane.showMessageDialog(null, "score=0");}	
+		}
 		// 4.  if the user's answer is correct
 		
 		// -- add one to their score 
@@ -18,5 +26,7 @@ public class QuizGame {
 		
 		// 6.  After all the questions have been asked, print the user's score 
 		
-	}
+	
+}
+}
 }
