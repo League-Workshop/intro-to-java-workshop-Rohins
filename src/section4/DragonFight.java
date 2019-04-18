ed144a5895fd06d1dbb6c3311202a356b2e25db7
Rohins <league@WTS-iMac-11.attlocal.net>
2019-04-18 14:23:10 -0700

    Day 4
    :smile:

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,29 +12,48 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+	int dragonHealth = 75;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+	int playerDamage= 0;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+	int dragonDamage = 0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+			String attack1 = JOptionPane.showInputDialog("Would you like to clap the dragon with a yell or kick");
 		// 9. If they typed in "yell":
-		
+			if (attack1.equalsIgnoreCase("yell")) {
+				dragonDamage=new Random().nextInt(10+1);
+				JOptionPane.showMessageDialog(null, "You did "+dragonDamage+" damage");
+				dragonHealth-=dragonDamage;
+				
+			} 
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable 
 			
 		// 10. If they typed in "kick":
-		
+			if (attack1.equalsIgnoreCase("kick")) {
+				dragonDamage=new Random().nextInt(25+1);
+				JOptionPane.showMessageDialog(null, "You did "+dragonDamage+" damage");
+				dragonHealth-=dragonDamage;
+				
+			} 
+			
+			
+			
+			if (attack1.equalsIgnoreCase("clap")) {
+				dragonDamage=new Random().nextInt(69+1);
+				JOptionPane.showMessageDialog(null, "You did "+dragonDamage+" damage");
+				dragonHealth-=dragonDamage;
+				
+			} 
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable
@@ -42,13 +61,18 @@ public class DragonFight {
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+			playerDamage=new Random().nextInt(35+1);
 		// 12. Subtract this number from the player's health
-		
+		playerHealth-=playerDamage;
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if (playerHealth<=0){
+			JOptionPane.showMessageDialog(null, "You died\nlol");	
+		}else{
+			if (dragonHealth<=0){
+				JOptionPane.showMessageDialog(null, "You clapped the dragon and you get a ton of gold!");	
+		}else {JOptionPane.showMessageDialog(null, "Your Health="+playerHealth+" The Dragons Health="+dragonHealth);
 			//-- Tell the user that they lost
 			
 		
@@ -62,4 +86,6 @@ public class DragonFight {
 			
 		}
 	}
-
+	}
+	}
+	}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -3,30 +3,52 @@ package section4;
 import javax.swing.JOptionPane;
 
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		int score=0;
-		// 2.  Ask the user a question 
-		String anwser1=JOptionPane.showInputDialog("Does curry taste good?");
-		// 3.  Use an if statement to check if their answer is correct
-		if(anwser1.equalsIgnoreCase("no")){
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		int points = 1;
+		// 2. Ask the user a question
+		String anwser1 = JOptionPane.showInputDialog("Does curry taste good?");
+		// 3. Use an if statement to check if their answer is correct
+		if (anwser1.equalsIgnoreCase("no")) {
 			JOptionPane.showMessageDialog(null, "Correct!");
-			JOptionPane.showMessageDialog(null, "score=1");}
-		else {
+			JOptionPane.showMessageDialog(null, score += points);
+		} else {
 			JOptionPane.showMessageDialog(null, "Wrong!");
-			JOptionPane.showMessageDialog(null, "score=0");}	
+			JOptionPane.showMessageDialog(null,  score -= points);
 		}
-		// 4.  if the user's answer is correct
+		String anwser2 = JOptionPane.showInputDialog("Is Marvel better than DC?");
+		if (anwser2.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			JOptionPane.showMessageDialog(null, score += points);
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+			JOptionPane.showMessageDialog(null,  score -= points);
+		}
+		String anwser3 = JOptionPane.showInputDialog("Do u no da wae?");
+		if (anwser3.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "yes");
+			JOptionPane.showMessageDialog(null, score += points);
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+			JOptionPane.showMessageDialog(null,  score -= points);
+		}
 		
-		// -- add one to their score 
 		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
-		// 6.  After all the questions have been asked, print the user's score 
 		
-	
-}
-}
+		
+		
+		
+	}
+	// 4. if the user's answer is correct
+
+	// -- add one to their score
+
+	// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+	// 6. After all the questions have been asked, print the user's score
+
 }
